13e84676abc7fdbcdc31e611f5622c7253709e0c
dams <d.macors@gmail.com>
2019-11-21 20:43:57 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@
 use Illuminate\Http\Request;
 use App\Http\Resources\Users as UserResource;
 use App\Users;
+use App\Lists;
+use App\Tasks;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,34 +24,86 @@ Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
 
 
-    Route::get('users', function() {
-        // If the Content-Type and Accept headers are set to 'application/json',
-        // this will return a JSON structure. This will be cleaned up later.
-        return Users::all();
-    });
+Route::get('users', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Users::all();
+});
 
-    Route::get('users/{id}', function($id) {
-     //   $arr = array();
-       // $arr['user'] = Users::find($id);
-     //   $arr['lists'] = Users::find($id)->lists;
-        return Users::find($id)->load('lists');
-    });
+Route::get('users/{id}', function($id) {
 
-    Route::post('users', function(Request $request) {
-        return Users::create($request->all);
-    });
+    return Users::find($id)->load('lists');
+});
 
-    Route::put('users/{id}', function(Request $request, $id) {
-        $Users = Users::findOrFail($id);
-        $Users->update($request->all());
+Route::post('users', function(Request $request) {
+    return Users::create($request->all());
+});
 
-        return $Users;
-    });
+Route::put('users/{id}', function(Request $request, $id) {
+    $Users = Users::findOrFail($id);
+    $Users->update($request->all());
 
-    Route::delete('users/{id}', function($id) {
-        Users::find($id)->delete();
+    return $Users;
+});
 
-        return 204;
-    });
-    Route::post('logout', 'Auth\LoginController@logout');
+Route::delete('users/{id}', function($id) {
+    Users::find($id)->delete();
 
+    return 204;
+});
+Route::post('logout', 'Auth\LoginController@logout');
+
+
+// Lists
+
+Route::get('lists', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Lists::all();
+});
+
+Route::get('lists/{id}', function($id) {
+
+    return Lists::find($id)->load('tasks');
+});
+
+Route::post('lists', function(Request $request) {
+    return Lists::create($request->all());
+});
+
+Route::put('lists/{id}', function(Request $request, $id) {
+    $Lists = Lists::findOrFail($id);
+    $Lists->update($request->all());
+
+    return $Lists;
+});
+
+Route::delete('lists/{id}', function($id) {
+    Lists::find($id)->delete();
+
+    return 204;
+});
+
+// Tasks
+
+Route::get('tasks/{id}', function($id) {
+
+    return Tasks::find($id);
+});
+
+Route::post('tasks', function(Request $request) {
+    return Tasks::create($request->all());
+});
+
+Route::put('tasks/{id}', function(Request $request, $id) {
+    $Tasks = Tasks::findOrFail($id);
+    $Tasks->update($request->all());
+
+    return $Tasks;
+});
+
+Route::delete('tasks/{id}', function($id) {
+    Tasks::find($id)->delete();
+
+    return 204;
+});
